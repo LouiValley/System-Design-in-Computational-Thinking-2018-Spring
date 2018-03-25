21aed9de9e9dc3bddbc280fb1ccb943a63e45937
habibi9 <mrj17@mails.tsinghua.edu.cn>
2018-03-25 23:33:39 +0800

    begining week 04
    Sucessfully completed mult program

--- team3/habibi9/04/mult/Mult.asm
+++ team3/habibi9/04/mult/Mult.asm
@@ -7,3 +7,57 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+//	i = 1
+//	loop{
+//		if i > y end
+//		x = x + y
+//		i = i +1
+
+@R0
+D=M
+@X
+M=D	//X = R0
+
+@R1
+D=M
+@Y
+M=D	//Y = R1
+
+@R2
+M=0
+
+@I
+M=1	//I = 1
+
+@SUM
+M = 0	//SUM = 0
+
+(LOOP)
+@I
+D=M
+@Y
+D=D-M
+@WRITE
+D;JGT	//IF I > Y END
+
+@X
+D=M
+@SUM
+M=M+D //SUM+=X
+
+@I
+M=M+1	//I+=1
+
+@LOOP
+0;JMP
+
+(WRITE)
+@SUM
+D=M
+@R2
+M=D
+
+(END)
+@END
+0;JMP
